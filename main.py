6565ae809a9802d69b01a334d5f3ab26b2f88b02
AlexVarvonets <Varvonets@yahoo.com>
2020-06-05 00:01:24 +0300

    refactoring started

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 from functions import show_menu_screen, run_game
 
-if __name__ == '__main__':
+
+# Game could be in 2 condition "WAITING" | "RUNNING"
+GAME_CONDITION = "WAITING"
+
+
+def draw_screen():
+    pass
+
+
+def start_game():
     while True:
+        if GAME_CONDITION == "RUNNING":
+            """
+            change snake position
+            draw snake
+            """
+        elif GAME_CONDITION == "WAITING":
+
+            """
+            waiting for pressing the start button
+            change game status
+            """
+
+        """
+        draw a screen
+        """
         show_menu_screen()
         run_game()
+
+
+if __name__ == '__main__':
+    start_game()
